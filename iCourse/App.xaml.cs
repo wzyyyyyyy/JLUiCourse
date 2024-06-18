bfa6865802e4eb55432069219670e44c105875c8
wzy <382271046@qq.com>
2024-06-18 13:36:38 +0800

    添加异常处理，减少消息输出带来的卡顿

--- iCourse/App.xaml.cs
+++ iCourse/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace iCourse
 {
@@ -7,6 +10,47 @@ namespace iCourse
     /// </summary>
     public partial class App : Application
     {
-    }
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // 捕获UI线程未处理的异常
+            Application.Current.DispatcherUnhandledException += DispatcherUnhandledExceptionHandler;
 
+            // 捕获非UI线程未处理的异常
+            AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionHandler;
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            // 清理事件处理程序
+            Application.Current.DispatcherUnhandledException -= DispatcherUnhandledExceptionHandler;
+            AppDomain.CurrentDomain.UnhandledException -= UnhandledExceptionHandler;
+        }
+
+        private void DispatcherUnhandledExceptionHandler(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            // 记录异常
+            LogException(e.Exception);
+            e.Handled = true; // 防止应用程序退出
+        }
+
+        private void UnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs e)
+        {
+            // 记录异常
+            if (e.ExceptionObject is Exception exception)
+            {
+                LogException(exception);
+            }
+        }
+
+        private void LogException(Exception exception)
+        {
+            string timestamp = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+            string logFileName = $"error_{timestamp}.log";
+            string logFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, logFileName);
+            string errorMessage = $"[{DateTime.Now}] {exception.ToString()}\n";
+
+            // 写入日志文件
+            File.AppendAllText(logFilePath, errorMessage);
+        }
+    }
 }
--- iCourse/Http.cs
+++ iCourse/Http.cs
@@ -26,7 +26,6 @@ namespace iCourse
             _client.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
             _client.DefaultRequestHeaders.Add("Host", "icourses.jlu.edu.cn");
             _client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36 Edg/103.0.1264.62");
-
             _retryPolicy = Policy
           .Handle<TaskCanceledException>()
           .Or<HttpRequestException>()
