dc60d3bdb77aa79e8cd54ad70a3b67661b645549
wzy <382271046@qq.com>
2024-06-25 20:21:54 +0800

    添加自动登录

---
+++ iCourse/UserCredentials.cs
@@ -0,0 +1,10 @@
+﻿namespace iCourse
+{
+    public class UserCredentials
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool AutoLogin { get; set; }
+    }
+
+}
