764ab802feffbeb5b1e19e2c8c8db869ffd3ce6f
wzy <382271046@qq.com>
2024-06-25 19:31:53 +0800

    重构一些代码，添加免责声明

--- iCourse/Web.cs
+++ iCourse/Web.cs
@@ -1,78 +1,57 @@
 ﻿using Newtonsoft.Json.Linq;
-using System.IO;
 using System.Net.Http;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
+
 using System.Windows;
-using System.Windows.Documents;
-using System.Windows.Input;
 
 namespace iCourse
 {
-    public class Web
+    public class Web(string username, string password)
     {
         private Http client;
 
-        private string AESKey;
-        private readonly string username;
-        private string password;
+        private string aesKey;
+        private readonly string username = username;
+        private string password = password;
         private string uuid;
         private string captcha;
         private string token;
-        private JObject login_response;
+        private JObject loginResponse;
         private BatchInfo batch;
 
-        public Web(string username, string password)
+        private async Task InitializeClientAsync()
         {
-            this.username = username;
-            this.password = password;
+            await RetrieveAESKeyAsync();
+            byte[] encryptedBytes = EncryptWithAES(password, aesKey);
+            password = Convert.ToBase64String(encryptedBytes);
         }
 
-        //code,msg
-        public async Task<(int, string)> LoginAsync()
+        private async Task<string> FetchCaptchaAsync()
         {
-            client = new Http(TimeSpan.FromSeconds(5));
-
-            // 获取AESKey
-            await GetAESKeyAsync();
-            byte[] encryptedBytes = EncryptWithAES(password, AESKey);
-            password = Convert.ToBase64String(encryptedBytes);
             client.SetOrigin("https://icourses.jlu.edu.cn");
             client.SetReferer("https://icourses.jlu.edu.cn/xsxk/profile/index.html");
 
-            // 获取验证码
-            var result = await client.HttpPostAsync("xsxk/auth/captcha", null);
+            const string captchaEndpoint = "xsxk/auth/captcha";
+
+            var result = await client.HttpPostAsync(captchaEndpoint, null);
             var json = JObject.Parse(result);
             uuid = json["data"]["uuid"].ToString();
             var captchaImage = json["data"]["captcha"].ToString();
-            captchaImage = captchaImage.Substring(captchaImage.IndexOf(",") + 1);
+            return captchaImage.Substring(captchaImage.IndexOf(",") + 1);
+        }
 
-            await GetCaptchaAsync(captchaImage);
-
-            // 登录
+        private async Task<(int code, string msg)> AttemptLoginAsync()
+        {
             var response = await PostLoginAsync();
-            json = JObject.Parse(response);
-            login_response = json;
+            var json = JObject.Parse(response);
+            loginResponse = json;
 
             int code = json["code"].ToObject<int>();
             string msg = json["msg"].ToString();
 
-            // 验证码错误
-            if (msg == "验证码错误")
-            {
-                MainWindow.Instance.WriteLine(msg);
-                (code, msg) = await LoginAsync();
-            }
-
-            // 登录失败
-            if (code != 200)
-            {
-                return (code, msg);
-            }
-
-            // 登录成功
-            if (json.ContainsKey("data"))
+            if (code == 200 && json.ContainsKey("data"))
             {
                 token = json["data"]["token"].ToString();
             }
@@ -80,25 +59,24 @@ namespace iCourse
             return (code, msg);
         }
 
-        public List<BatchInfo> GetBatchInfo()
+        public async Task<(int code, string msg)> LoginAsync()
         {
-            var batchInfos = new List<BatchInfo>();
-            login_response["data"]["student"]["electiveBatchList"].ToList().ForEach(batch =>
+            client = new Http(TimeSpan.FromSeconds(5));
+
+            await InitializeClientAsync();
+
+            string captchaImage = await FetchCaptchaAsync();
+            await DisplayCaptchaAsync(captchaImage);
+
+            var (code, msg) = await AttemptLoginAsync();
+
+            if (msg == "验证码错误")
             {
-                var batchInfo = new BatchInfo
-                {
-                    batchCode = batch["code"].ToString(),
-                    batchName = batch["name"].ToString(),
-                    beginTime = batch["beginTime"].ToString(),
-                    endTime = batch["endTime"].ToString(),
-                    tacticName = batch["tacticName"].ToString(),
-                    noSelectReason = batch["noSelectReason"].ToString(),
-                    typeName = batch["typeName"].ToString(),
-                    canSelect = batch["canSelect"].ToString() != "0" //你鸡为什么用字符串，逆天
-                };
-                batchInfos.Add(batchInfo);
-            });
-            return batchInfos;
+                MainWindow.Instance.WriteLine(msg);
+                (code, msg) = await LoginAsync();
+            }
+
+            return (code, msg);
         }
 
         private byte[] EncryptWithAES(string plainText, string aesKey)
@@ -115,25 +93,23 @@ namespace iCourse
 
                 byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
 
-                // 加密
                 byte[] encryptedBytes = encryptor.TransformFinalBlock(plainBytes, 0, plainBytes.Length);
 
                 return encryptedBytes;
             }
         }
 
-        private async Task<string> GetAESKeyAsync()
+        private async Task RetrieveAESKeyAsync()
         {
             var response = await client.HttpGetAsync("");
             var match = Regex.Match(response, "loginVue\\.loginForm\\.aesKey\\s*=\\s*\"([^\"]+)\"");
             if (match.Success)
             {
-                AESKey = match.Groups[1].Value;
-                return AESKey;
+                aesKey = match.Groups[1].Value;
             }
             else
             {
-                throw new InvalidOperationException("aesKey not found in content.");
+                throw new InvalidOperationException("AES key not found in content.");
             }
         }
 
@@ -149,7 +125,7 @@ namespace iCourse
             return response;
         }
 
-        private async Task<string> GetCaptchaAsync(string base64Image)
+        private async Task DisplayCaptchaAsync(string base64Image)
         {
             await MainWindow.Instance.Dispatcher.InvokeAsync(() =>
             {
@@ -157,15 +133,35 @@ namespace iCourse
                 captchaWindow.ShowDialog();
                 captcha = CaptchaWindow.Captcha;
             });
-            return captcha;
         }
 
         public JObject GetLoginResponse()
         {
-            return login_response;
+            return loginResponse;
         }
 
-        public async void SetBatchID(BatchInfo batch)
+        public List<BatchInfo> GetBatchInfo()
+        {
+            var batchInfos = new List<BatchInfo>();
+            loginResponse["data"]["student"]["electiveBatchList"].ToList().ForEach(batch =>
+            {
+                var batchInfo = new BatchInfo
+                {
+                    batchId = batch["code"].ToString(),
+                    batchName = batch["name"].ToString(),
+                    beginTime = batch["beginTime"].ToString(),
+                    endTime = batch["endTime"].ToString(),
+                    tacticName = batch["tacticName"].ToString(),
+                    noSelectReason = batch["noSelectReason"].ToString(),
+                    typeName = batch["typeName"].ToString(),
+                    canSelect = batch["canSelect"].ToString() != "0"
+                };
+                batchInfos.Add(batchInfo);
+            });
+            return batchInfos;
+        }
+
+        public async Task SetBatchIDAsync(BatchInfo batch)
         {
             this.batch = batch;
             client.SetOrigin("https://icourses.jlu.edu.cn");
@@ -173,13 +169,13 @@ namespace iCourse
             client.AddHeader("Authorization", token);
             var response = await client.HttpPostAsync("xsxk/elective/user", new FormUrlEncodedContent(new Dictionary<string, string>
             {
-                {"batchId", batch.batchCode}
+                {"batchId", batch.batchId}
             }));
             var json = JObject.Parse(response);
             if (json["code"].ToObject<int>() == 200)
             {
                 MainWindow.Instance.WriteLine("选课批次设置成功");
-                MainWindow.Instance.WriteLine("已选批次:"+ batch.batchName);
+                MainWindow.Instance.WriteLine("已选批次:" + batch.batchName);
             }
             else
             {
@@ -188,18 +184,16 @@ namespace iCourse
 
             client.SetReferer("https://icourses.jlu.edu.cn/xsxk/profile/index.html");
 
-            await client.HttpGetAsync("xsxk/elective/grablessons?batchId=" + batch.batchCode);
+            await client.HttpGetAsync("xsxk/elective/grablessons?batchId=" + batch.batchId);
         }
 
-        public async Task<List<Course>> GetFavoriteCourses()
+        public async Task<List<Course>> GetFavoriteCoursesAsync()
         {
-            List<Course> list = new List<Course>();
+            List<Course> coursesList = new List<Course>();
 
-            client.SetReferer("https://icourses.jlu.edu.cn/xsxk/elective/grablessons?batchId="+batch.batchCode);
+            client.SetReferer("https://icourses.jlu.edu.cn/xsxk/elective/grablessons?batchId=" + batch.batchId);
             var response = await client.HttpPostAsync("xsxk/sc/clazz/list", null);
             var json = JObject.Parse(response);
-
-            var msg = String.Empty;
 
             if (json["code"].ToObject<int>() == 200)
             {
@@ -213,8 +207,7 @@ namespace iCourse
                         secretVal = course["secretVal"].ToString(),
                         clazzType = course["teachingClassType"].ToString()
                     };
-                    msg+= courseInfo.courseName + "\n";
-                    list.Add(courseInfo);
+                    coursesList.Add(courseInfo);
                 }
             }
             else
@@ -222,11 +215,11 @@ namespace iCourse
                 MainWindow.Instance.WriteLine(json["msg"].ToString());
             }
 
-            MainWindow.Instance.WriteLine("收藏中的课程:\n" + msg);
-            return list;
+            MainWindow.Instance.WriteLine("收藏中的课程:\n" + string.Join("\n", coursesList.Select(c => c.courseName)));
+            return coursesList;
         }
 
-        public async void SelectCourse(Course course)
+        public async Task SelectCourseAsync(Course course)
         {
             while (true)
             {
@@ -234,7 +227,7 @@ namespace iCourse
                 {
                     {"clazzId", course.courseID},
                     {"secretVal", course.secretVal},
-                    {"clazzType",course.clazzType }
+                    {"clazzType", course.clazzType }
                 }));
 
                 var json = JObject.Parse(response);
