c81afe776fe96f63e983010c9fba85a50a659f1c
wzy <59381521+wzyyyyyyy@users.noreply.github.com>
2024-07-12 08:27:05 +0800

    修改http类

--- iCourse/Http.cs
+++ iCourse/Http.cs
@@ -1,89 +1,68 @@
 ﻿using Polly;
 using Polly.Retry;
-using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace iCourse
 {
-    class Http
+    class Http : HttpClient
     {
-        private readonly HttpClient _client;
         private readonly AsyncRetryPolicy _retryPolicy;
 
         public Http(TimeSpan timeout)
+            : base(new HttpClientHandler { UseCookies = true })
         {
-            HttpClientHandler handler = new HttpClientHandler
-            {
-                UseCookies = true
-            };
+            Timeout = timeout;
+            BaseAddress = new Uri("https://icourses.jlu.edu.cn");
 
-            _client = new HttpClient(handler)
-            {
-                Timeout = timeout,
-                BaseAddress = new Uri("https://icourses.jlu.edu.cn")
-            };
+            DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+            DefaultRequestHeaders.Add("Host", "icourses.jlu.edu.cn");
+            DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36 Edg/103.0.1264.62");
 
-            _client.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
-            _client.DefaultRequestHeaders.Add("Host", "icourses.jlu.edu.cn");
-            _client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36 Edg/103.0.1264.62");
             _retryPolicy = Policy
-          .Handle<TaskCanceledException>()
-          .Or<HttpRequestException>()
-          .WaitAndRetryForeverAsync(
-              retryAttempt =>
-              {
-                  var waitTime = TimeSpan.FromMilliseconds(Math.Pow(2, Math.Min(retryAttempt, 8))); // 上限为256ms
-                  MainWindow.Instance.WriteLine($"重试第 {retryAttempt} 次，等待时间 {waitTime.TotalSeconds} 秒");
-                  return waitTime;
-              },
-              onRetry: (exception, timeSpan) =>
-              {
-                  if (exception is TaskCanceledException)
-                  {
-                      MainWindow.Instance.WriteLine($"重试超时，等待 {timeSpan.TotalSeconds} 秒后重试。");
-                  }
-                  else
-                  {
-                      MainWindow.Instance.WriteLine($"发生异常：{exception.Message}，等待 {timeSpan.TotalSeconds} 秒后重试。");
-                  }
-              });
+                .Handle<TaskCanceledException>()
+                .Or<HttpRequestException>()
+                .WaitAndRetryForeverAsync(
+                    retryAttempt =>
+                    {
+                        var waitTime = TimeSpan.FromMilliseconds(Math.Pow(2, Math.Min(retryAttempt, 8))); // 上限为256ms
+                        MainWindow.Instance.WriteLine($"重试第 {retryAttempt} 次，等待时间 {waitTime.TotalSeconds} 秒");
+                        return waitTime;
+                    },
+                    onRetry: (exception, timeSpan) =>
+                    {
+                        if (exception is TaskCanceledException)
+                        {
+                            MainWindow.Instance.WriteLine($"重试超时，等待 {timeSpan.TotalSeconds} 秒后重试。");
+                        }
+                        else
+                        {
+                            MainWindow.Instance.WriteLine($"发生异常：{exception.Message}，等待 {timeSpan.TotalSeconds} 秒后重试。");
+                        }
+                    });
         }
 
         public void SetOrigin(string origin)
         {
-            if (_client.DefaultRequestHeaders.Contains("Origin"))
-            {
-                _client.DefaultRequestHeaders.Remove("Origin");
-            }
-            _client.DefaultRequestHeaders.Add("Origin", origin);
+            DefaultRequestHeaders.Remove("Origin");
+            DefaultRequestHeaders.Add("Origin", origin);
         }
 
         public void SetReferer(string referer)
         {
-            if (_client.DefaultRequestHeaders.Contains("Referer"))
-            {
-                _client.DefaultRequestHeaders.Remove("Referer");
-            }
-            _client.DefaultRequestHeaders.Add("Referer", referer);
+            DefaultRequestHeaders.Remove("Referer");
+            DefaultRequestHeaders.Add("Referer", referer);
         }
 
         public void AddHeader(string key, string value)
         {
-            if (_client.DefaultRequestHeaders.Contains(key))
-            {
-                _client.DefaultRequestHeaders.Remove(key);
-            }
-            _client.DefaultRequestHeaders.Add(key, value);
+            DefaultRequestHeaders.Remove(key);
+            DefaultRequestHeaders.Add(key, value);
         }
 
         public void AddCookie(string cookie)
         {
-            var cookies = new CookieContainer();
-            var uri = new Uri("https://icourses.jlu.edu.cn");
-            cookies.Add(uri, new Cookie("your_cookie_name", "your_cookie_value"));
-
-            var handler = new HttpClientHandler { CookieContainer = cookies };
-            _client.DefaultRequestHeaders.Add("Cookie", cookie);
+            DefaultRequestHeaders.Add("Cookie", cookie);
         }
 
         public async Task<string> HttpGetAsync(string url)
@@ -92,7 +71,7 @@ namespace iCourse
             {
                 var request = new HttpRequestMessage(HttpMethod.Get, url);
 
-                using var response = await _client.SendAsync(request);
+                using var response = await SendAsync(request);
                 response.EnsureSuccessStatusCode();
                 return await response.Content.ReadAsStringAsync();
             });
@@ -107,7 +86,7 @@ namespace iCourse
                     Content = content
                 };
 
-                using var response = await _client.SendAsync(request);
+                using var response = await SendAsync(request);
                 response.EnsureSuccessStatusCode();
                 return await response.Content.ReadAsStringAsync();
             });
