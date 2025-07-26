8b9f26a163e3476118b1475d102ce567d018563f
wzy <382271046@qq.com>
2025-07-26 11:56:25 +0800

    init

---
+++ iCourse/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace iCourse.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ iCourse/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace iCourse.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
