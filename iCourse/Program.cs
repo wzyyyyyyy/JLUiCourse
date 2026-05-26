09efcf88971d044b1d468715b978b357c057b6e6
wzy <59381521+wzyyyyyyy@users.noreply.github.com>
2026-05-26 18:51:24 +0800

    chore: convert project shell to Avalonia

---
+++ iCourse/Program.cs
@@ -0,0 +1,21 @@
+using Avalonia;
+using System;
+
+namespace iCourse;
+
+internal static class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
+
+    public static AppBuilder BuildAvaloniaApp()
+    {
+        return AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
+    }
+}
